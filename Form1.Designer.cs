ac1519acd19657f1a4db39afd275968dcc01d089
Jacob Lehenbauer <jlehenbauer@lithionbattery.com>
2023-02-20 15:52:24 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace DataMatrixCV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelRectangleCoordinates = new System.Windows.Forms.Label();
+            this.labelDMData = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(684, 376);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(265, 394);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(161, 44);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Begin Capture";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelRectangleCoordinates
+            // 
+            this.labelRectangleCoordinates.AutoSize = true;
+            this.labelRectangleCoordinates.Location = new System.Drawing.Point(43, 409);
+            this.labelRectangleCoordinates.Name = "labelRectangleCoordinates";
+            this.labelRectangleCoordinates.Size = new System.Drawing.Size(31, 13);
+            this.labelRectangleCoordinates.TabIndex = 2;
+            this.labelRectangleCoordinates.Text = "(0, 0)";
+            // 
+            // labelDMData
+            // 
+            this.labelDMData.AutoSize = true;
+            this.labelDMData.Location = new System.Drawing.Point(520, 409);
+            this.labelDMData.Name = "labelDMData";
+            this.labelDMData.Size = new System.Drawing.Size(33, 13);
+            this.labelDMData.TabIndex = 3;
+            this.labelDMData.Text = "None";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 450);
+            this.Controls.Add(this.labelDMData);
+            this.Controls.Add(this.labelRectangleCoordinates);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Data Matrix Capture";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelRectangleCoordinates;
+        private System.Windows.Forms.Label labelDMData;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using ZXing;
+using ZXing.Common;
+using ZXing.Datamatrix;
+
+namespace DataMatrixCV
+{
+    public partial class Form1 : Form
+    {
+        // variables for video capture
+        private VideoCapture capture;
+        private Mat frame;
+        private Bitmap image;
+        private Thread camera;
+        private bool isCameraRunning = false;
+        private BarcodeReader BReader = new BarcodeReader();
+        private DataMatrixReader DReader = new DataMatrixReader();
+        private Rectangle rectangle;
+        private string coord;
+        private string label;
+        private ImageConverter converter = new ImageConverter();
+        private void CaptureCamera()
+        {
+            camera = new Thread(new ThreadStart(CaptureCameraCallback));
+            camera.Start();
+        }
+
+        private void CaptureCameraCallback()
+        {
+            frame = new Mat();
+            capture = new VideoCapture(0);
+            capture.AutoFocus = true;
+            capture.Open(0);
+
+            if (capture.IsOpened())
+            {
+                while (isCameraRunning)
+                {
+                    capture.Read(frame);
+                    image = BitmapConverter.ToBitmap(frame);
+                    if (pictureBox1.Image != null)
+                    {
+                        pictureBox1.Image.Dispose();
+                    }
+
+                    pictureBox1.Image = image;
+                    //byte[] intArray = ImageToByte(image);
+                    //LuminanceSource lumSource = new RGBLuminanceSource(intArray, image.Width, image.Height);
+                    //BinaryBitmap operableImage = new BinaryBitmap(new HybridBinarizer(lumSource));
+                    //var result = DReader.decode(operableImage);
+
+                    Bitmap operableImage = (Bitmap)image.Clone();
+                    var result = BReader.Decode(operableImage);
+                    if (result != null)
+                    {
+                        coord = $"({result.ResultPoints[1].X}, {result.ResultPoints[1].Y})";
+                        label = result.Text;
+                        try
+                        {
+                            rectangle = new System.Drawing.Rectangle(
+                                (int) result.ResultPoints[1].X, 
+                                pictureBox1.Image.Height - (int) result.ResultPoints[1].Y,
+                                (int) (result.ResultPoints[2].X - result.ResultPoints[0].X),
+                                (int) (result.ResultPoints[2].Y - result.ResultPoints[0].Y));
+                        }
+                        catch { }
+                        //MessageBox.Show($"Data matrix found: {result}", "Decoded data matrix");
+                    }
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            CaptureCamera();
+            buttonStart.Text = "End capture";
+            isCameraRunning = true;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (buttonStart.Text == "End capture")
+            {
+                isCameraRunning = false;
+                capture.Release();
+                buttonStart.Text = "Begin capture";
+                labelRectangleCoordinates.Text = "";
+                labelDMData.Text = "";
+            }
+            else
+            {
+                CaptureCamera();
+                buttonStart.Text = "End capture";
+                isCameraRunning = true;
+            }
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            using (Pen pen = new Pen(Color.LimeGreen, 2))
+            {
+                e.Graphics.DrawRectangle(pen, rectangle);
+            }
+            labelRectangleCoordinates.Text = coord;
+            labelDMData.Text = label;
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            isCameraRunning = false;
+            try
+            {
+                capture.Release();
+                camera.Abort();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        protected static byte[] ImageToByte(Bitmap img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Bmp);
+                return stream.ToArray();
+            }
+        }
+    }
+}
